e6caed3bc0ec14558fa9d698590a6fa04c8a0d9e
kevinruan <kev2003r@gmail.com>
2025-02-14 23:13:36 -0800

    Added Reset Functionality

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BallController : MonoBehaviour
 {
     
@@ -19,14 +20,25 @@ public class BallController : MonoBehaviour
         transform.parent = ballAnchor;
         transform.localPosition = Vector3.zero;
         ballRB.isKinematic = true;
+        ResetBall();
     }
 
-    private void LaunchBall() {
+    private void LaunchBall()
+    {
         if (isBallLaunched) return;
         transform.parent = null;
         isBallLaunched = true;
         ballRB.isKinematic = false;
         ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
         launchIndicator.gameObject.SetActive(false);
+    }
+
+    public void ResetBall()
+    {
+        isBallLaunched = false;
+        ballRB.isKinematic = true;
+        launchIndicator.gameObject.SetActive(true);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,57 @@
 using TMPro;
 using UnityEngine;
 
-public class GameBehaviour : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
     [SerializeField] private TextMeshProUGUI scoreText;
-    private FallTrigger[] pins;
+    private FallTrigger[] fallTriggers;
+    private GameObject pinObjects;
+
 
     private void Start()
     {
-        pins = FindObjectsOfType<FallTrigger>(true);
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
 
-        foreach (FallTrigger pin in pins)
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        if (pinObjects)
+        {
+            foreach (Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, pinAnchor.transform.rotation, transform);
+        fallTriggers = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (FallTrigger pin in fallTriggers)
         {
             pin.OnPinFall.AddListener(IncrementScore);
         }
     }
-
     private void IncrementScore()
     {
         score++;
         scoreText.text = $"Score: {score}";
     }
 }
+        // pins = FindObjectsOfType<FallTrigger>(true);
+
+        // foreach (FallTrigger pin in pins)
+        // {
+        //     pin.OnPinFall.AddListener(IncrementScore);
+        // }
